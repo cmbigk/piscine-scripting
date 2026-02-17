1f5eeceb26aa27dde11ed06870ac7cad8f814df0
chmyint <bigk89865@gmail.com>
2026-02-17 20:27:22 +0200

    122A

---
+++ file-details.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# List files in hard-perm folder with custom format:
+# permissions date time filename
+
+# ls -l                        : long listing format
+# --time-style='+%F %R'        : custom date/time format
+#   %F                         : full ISO date (YYYY-MM-DD)
+#   %R                         : 24-hour time (HH:MM)
+# hard-perm                    : target directory
+# | sed '1d'                   : pipe to sed, delete first line (total)
+# | awk '{print $1, $6, $7, $8}' : print columns 1 (perms), 6 (date), 7 (time), 8 (name)
+
+ls -l --time-style='+%F %R' hard-perm | sed '1d' | awk '{print $1, $6, $7, $8}'
