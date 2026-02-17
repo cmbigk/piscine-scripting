142c0f4b89acaf4d02a6ef87ae099a1308b2b0f7
chmyint <bigk89865@gmail.com>
2026-02-17 23:49:23 +0200

    123F

--- set-internal-vars.sh
+++ set-internal-vars.sh
@@ -6,3 +6,16 @@ MY_PI=3.142
 MY_ARR="one, two, three, four, five"
 
 printf "%s\n%s\n%s\n%s\n" "$MY_MESSAGE" "$MY_NUM" "$MY_PI" "$MY_ARR"
+
+
+#"%s\n%s\n%s\n%s\n"
+  │   │   │   │
+  │   │   │   └── %s = 4th argument (MY_ARR)
+  │   │   │       \n = newline after it
+  │   │   └────── %s = 3rd argument (MY_PI)
+  │   │           \n = newline after it
+  │   └────────── %s = 2nd argument (MY_NUM)
+  │               \n = newline after it
+  └────────────── %s = 1st argument (MY_MESSAGE)
+                  \n = newline after it
+
