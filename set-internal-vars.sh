a571b55b5fb1c2d2842d71987029c019034563f3
chmyint <bigk89865@gmail.com>
2026-02-17 23:40:48 +0200

    123F

--- set-internal-vars.sh
+++ set-internal-vars.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
+
 MY_MESSAGE="Hello World"
 MY_NUM=100
 MY_PI=3.142
-MY_ARR=(one, two, three, four, five)
-echo $MY_MESSAGE
-echo $MY_NUM
-echo $MY_PI
-echo ${MY_ARR[*]}
+MY_ARR="one, two, three, four, five"
+
+printf "%s\n%s\n%s\n%s\n" "$MY_MESSAGE" "$MY_NUM" "$MY_PI" "$MY_ARR"
