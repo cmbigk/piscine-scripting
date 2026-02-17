eeab65d2b04f72c6392ea19420e0493542c6e675
chmyint <bigk89865@gmail.com>
2026-02-17 03:14:05 +0200

    111A

---
+++ skip-lines.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#-n = suppress automatic printing (only print when we say p)
+# = print current line (line 1)
+#; = command separator
+#n = read next line into buffer (line 2) without printing
+#Repeat: p prints line 3, n skips line 4, etc.
+
+
+ls -l | sed -n 'p;n'
