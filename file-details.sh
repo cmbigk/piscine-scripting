6e17a283ec0491620b2fb9ee49ad55f73ceccf88
chmyint <bigk89865@gmail.com>
2026-02-17 22:57:43 +0200

    122E

--- file-details.sh
+++ file-details.sh
@@ -9,5 +9,6 @@
 # hard-perm                    : target directory
 # | sed '1d'                   : pipe to sed, delete first line (total)
 # | awk '{print $1, $6, $7, $8}' : print columns 1 (perms), 6 (date), 7 (time), 8 (name)
+#!/bin/bash
 
-TZ=utc ls -l --time-style='+%F %R' hard-perm | sed '1d' | awk '{print $1, $6, $7, $8}'
+ls -l --time-style='+%F %R' hard-perm | sed '1d' | awk '{print $1, $6, $7, $8}'
