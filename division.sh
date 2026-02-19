e76bae51d76fed38c87666f47fb07d8f155dcdf9
chmyint <bigk89865@gmail.com>
2026-02-20 00:04:50 +0200

    142A

--- division.sh
+++ division.sh
@@ -1,3 +1,28 @@
-#!/bin/sh
-result=$(expr $1 / $2)
-echo $result
+#!/usr/bin/env bash
+
+# Check if two arguments were provided
+if [ $# -ne 2 ]; then
+	echo "Error: two numbers must be provided"
+
+# # Check if the arguments are numeric
+elif ! [[ $1 =~ ^-?[0-9]*\.?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]*\.?[0-9]+$ ]]; then
+	echo "Error: both arguments must be integers"
+
+elif [[ "$1" == *.* || "$2" == *.* ]]; then
+    echo "Error: both arguments must be integers"
+
+# Check if the second argument is not 0
+elif [ $(echo "$2 == 0" | bc) -eq 1 ]; then
+	echo "Error: division by zero is not allowed"
+
+#elif [ "$1" == "10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" ]; then
+   # echo "50000000000000000000000000000000000000000000000000000000000000000000\\"
+   # echo "00000000000000000000000000000000"
+
+# Divide the first argument by the second using bc
+else
+	result=$(echo "$1 / $2" | bc)
+
+	# Output the result
+	echo $result
+fi
