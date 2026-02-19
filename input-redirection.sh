ed2e3d8129df11879bb62efe8a324a6b50962205
chmyint <bigk89865@gmail.com>
2026-02-19 16:29:21 +0200

    133A

---
+++ input-redirection.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# The goal is for the resulting file to read a heredoc
+echo "cat -e <<EOF
+The current directory is: \$PWD
+The default paths are: \$PATH
+The current user is: \$USERNAME
+EOF
+" > show-info.sh
+
+chmod +x show-info.shs
