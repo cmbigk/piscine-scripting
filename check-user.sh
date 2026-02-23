b50cefade13909594b0038905c0ca95f4639d35a
chmyint <bigk89865@gmail.com>
2026-02-23 04:16:21 +0200

    add

--- check-user.sh
+++ check-user.sh
@@ -32,3 +32,23 @@ case "$FLAG" in
     fi
     ;;
 esac
+
+
+
+#!/bin/bash
+
+#if [ $# != 2 ]; then
+	>&2 echo "Error: expect 2 arguments"
+	exit 1
+#elif [[ $1 == "-e" ]]; then
+    if id "$2" &>/dev/null; then
+        echo yes
+    else
+        echo no
+    fi
+#elif [[ $1 == "-i" ]]; then
+    id "$2"
+#else
+	>&2 echo "Error: unknown flag"
+	exit 1
+#fi
