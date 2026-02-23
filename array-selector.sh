eb557ba5d4be79ceda046dc1a3d7186985f10436
chmyint <bigk89865@gmail.com>
2026-02-23 02:23:38 +0200

    array-select

--- array-selector.sh
+++ array-selector.sh
@@ -6,6 +6,7 @@ if [[ "$#" != 1 ]] || ! [[ "$1" =~ ^[0-9]+$ ]] || [[ "$1" < 1 ]] || [[ "$1" > 5 
         exit 0
 fi
 
+POS="$1"
 COLORS=(red blue green white black)
 
-echo "${COLORS[$(( $1 - 1 ))]}"
+echo "${COLORS[$(( $POS - 1 ))]}"
