63c458b8f3981f8d56c046b1dc64adbbe833666c
chmyint <bigk89865@gmail.com>
2026-02-24 15:18:22 +0200

    162A

---
+++ numerical_operations.py
@@ -0,0 +1,25 @@
+import math
+
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def power(a,b):
+    return a ** b
+
+def square_root(a):
+    return math.sqrt(a)
+
+
+"""
+print(add(2, 2))
+print(subtract(10, 5))
+print(multiply(3, 4))
+print(power(3, 3))
+print(square_root(3))
+"""
